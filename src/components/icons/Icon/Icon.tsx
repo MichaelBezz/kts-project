6bc35e08f5ce7d7304ef7227b2933e98a4fd3b56
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-06 15:33:31 +0300

    feat: добавляет Icon

---
+++ src/components/icons/Icon/Icon.tsx
@@ -0,0 +1,34 @@
+import cn from 'classnames';
+import * as React from 'react';
+import styles from './Icon.module.scss';
+
+export type IconProps = React.SVGAttributes<SVGElement> & {
+  className?: string;
+  width?: number;
+  height?: number;
+  color?: 'primary' | 'secondary' | 'accent';
+};
+
+const Icon: React.FC<React.PropsWithChildren<IconProps>> = ({
+  className,
+  width = 24,
+  height = 24,
+  color,
+  children,
+  ...props
+}) => {
+  return(
+    <svg
+      className={cn(styles.icon, {[styles[`icon--${color}`]]: color}, className)}
+      width={width}
+      height={height}
+      preserveAspectRatio="xMidYMid meet"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      {children}
+    </svg>
+  );
+}
+
+export default Icon;
