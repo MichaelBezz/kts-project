1393d266b8a590c834058bf0c3547e7fc64cfe64
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-06 15:31:20 +0300

    fix(Layout): мелкие правки и рефакторинг

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from 'components/Header';
 import styles from './Layout.module.scss';
 
 const Layout: React.FC = () => {
   return (
     <div className={styles.layout}>
-      <header className={styles.header}></header>
+      <Header />
 
       <main>
         <Outlet />
