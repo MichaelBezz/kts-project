c61f24949b705d316c6f03a443ffc9c224c1e4ba
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-06 19:13:57 +0300

    fix: исправляет стилизацию иконок

--- src/components/icons/UserIcon/UserIcon.tsx
+++ src/components/icons/UserIcon/UserIcon.tsx
@@ -4,8 +4,8 @@ import Icon, { IconProps } from '../Icon';
 const UserIcon: React.FC<IconProps> = (props) => {
   return (
     <Icon fill="none" viewBox="0 0 30 30" {...props}>
-      <path stroke="#151411" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" d="M15 15C18.4518 15 21.25 12.2018 21.25 8.75C21.25 5.29822 18.4518 2.5 15 2.5C11.5482 2.5 8.75 5.29822 8.75 8.75C8.75 12.2018 11.5482 15 15 15Z"/>
-      <path stroke="#151411" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" d="M25.7374 27.5C25.7374 22.6625 20.9249 18.75 14.9999 18.75C9.07495 18.75 4.26245 22.6625 4.26245 27.5"/>
+      <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" d="M15 15C18.4518 15 21.25 12.2018 21.25 8.75C21.25 5.29822 18.4518 2.5 15 2.5C11.5482 2.5 8.75 5.29822 8.75 8.75C8.75 12.2018 11.5482 15 15 15Z"/>
+      <path stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" d="M25.7374 27.5C25.7374 22.6625 20.9249 18.75 14.9999 18.75C9.07495 18.75 4.26245 22.6625 4.26245 27.5"/>
     </Icon>
   );
 }
