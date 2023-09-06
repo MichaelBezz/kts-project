5c80f566b250792c1e7429b66df931e548a33620
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-06 11:27:59 +0300

    feat: добавляет роутер

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from 'config/routes';
+import MainPage from './pages/MainPage';
 
 const App: React.FC = () => {
-  return (<div />);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/App/pages/MainPage/MainPage.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const MainPage: React.FC = () => {
+  return(<div />);
+};
+
+export default MainPage;
