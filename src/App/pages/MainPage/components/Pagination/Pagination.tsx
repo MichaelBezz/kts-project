23df30a01f179c03abec338935fe127b95929302
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-07 19:17:23 +0300

    feat: добавляет Pagination

---
+++ src/App/pages/MainPage/components/Pagination/Pagination.tsx
@@ -0,0 +1,59 @@
+import cn from 'classnames';
+import * as React from 'react';
+import Text from 'components/Text';
+import ArrowIcon from 'components/icons/ArrowIcon';
+import styles from './Pagination.module.scss';
+
+export type PaginationProps = {
+  className?: string;
+};
+
+const Pagination: React.FC<PaginationProps> = ({ className }) => {
+  return (
+    <div className={cn(styles['pagination'], className)}>
+      <ul className={styles['pagination__list']}>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button" disabled>
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">
+              <ArrowIcon width={32} height={32} />
+            </Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={cn(styles['pagination__button'], styles['pagination__button--current'])} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">1</Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">2</Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">3</Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">...</Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">10</Text>
+          </button>
+        </li>
+        <li className={styles['pagination__item']}>
+          <button className={styles['pagination__button']} type="button">
+            <Text className={styles['pagination__button-text']} tag="span" view="p-18" weight="medium">
+              <ArrowIcon width={32} height={32} />
+            </Text>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/App/pages/MainPage/components/ProductList/ProductList.tsx
+++ src/App/pages/MainPage/components/ProductList/ProductList.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Button from 'components/Button';
 import Card from 'components/Card';
 import Text from 'components/Text';
+import Pagination from '../Pagination';
 import styles from './ProductList.module.scss';
 
 export type ProductListProps = {
@@ -124,6 +125,8 @@ const ProductList: React.FC<ProductListProps> = ({ className }) => {
             />
           </li>
         </ul>
+
+        <Pagination className={styles['product-list__pagination']} />
       </div>
     </section>
   );
