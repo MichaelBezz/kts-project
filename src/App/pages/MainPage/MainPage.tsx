014ee0f02dcf579e59e7f4821170232298c1e3bb
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-07 17:30:29 +0300

    feat: добавляет ProductList

--- src/App/pages/MainPage/MainPage.tsx
+++ src/App/pages/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import Intro from './components/Intro';
+import ProductList from './components/ProductList';
 import styles from './MainPage.module.scss';
 
 const MainPage: React.FC = () => {
   return(
     <div className={styles['main-page']}>
       <Intro className={styles['main-page__intro']} />
+      <ProductList className={styles['main-page__products']} />
     </div>
   );
 };
---
+++ src/App/pages/MainPage/components/ProductList/ProductList.tsx
@@ -0,0 +1,99 @@
+import cn from 'classnames';
+import * as React from 'react';
+import Button from 'components/Button';
+import Card from 'components/Card';
+import Text from 'components/Text';
+import styles from './ProductList.module.scss';
+
+export type ProductListProps = {
+  className?: string;
+};
+
+const ProductList: React.FC<ProductListProps> = ({ className }) => {
+  return (
+    <section className={cn(styles['product-list'], className)}>
+      <div className="container">
+        <div className={styles['product-list__header']}>
+          <Text className={styles['product-list__title']} tag="h2" view="p-32">
+            Total Product
+          </Text>
+
+          <Text className={styles['product-list__count']} tag="p" view="p-20" weight="bold" color="accent">
+            184
+          </Text>
+        </div>
+
+        <ul className={styles['product-list__body']}>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+          <li className={styles['product-list__item']}>
+            <Card
+              image={'https://placehold.co/360x360'}
+              captionSlot={'Chair'}
+              title={'White Aesthetic Chair'}
+              subtitle={'Combination of wood and wool'}
+              contentSlot={'$63.47'}
+              onClick={() => {}}
+              actionSlot={<Button>Add to Cart</Button>}
+            />
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default ProductList;
