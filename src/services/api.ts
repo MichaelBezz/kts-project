389d9b4fd16536468c0be6d1c6c08e0e67c1c403
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-14 22:12:35 +0300

    fix: мелкие правки и рефакторинг

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import { APIRoute } from 'config/api-route';
-import { ProductModel } from 'store/models/product';
-import { TCategory } from 'types/category';
+import { ProductApi, CategoryApi } from 'store/models/product';
 
 const BACKEND_URL = 'https://api.escuelajs.co/api/v1';
 const REQUEST_TIMEOUT = 5000;
@@ -24,24 +23,24 @@ export const createAPI = (): AxiosInstance => {
 
 export const api = createAPI();
 
-export const fetchProducts = async (offset = 0, limit = 0): Promise<ProductModel[]> => {
-  const { data } = await api.get<ProductModel[]>(`${APIRoute.products}?offset=${offset}&limit=${limit}`);
+export const fetchProducts = async (offset = 0, limit = 0): Promise<ProductApi[]> => {
+  const { data } = await api.get<ProductApi[]>(`${APIRoute.products}?offset=${offset}&limit=${limit}`);
   return data;
 };
 
-export const fetchProductById = async (id: string): Promise<ProductModel> => {
-  const { data } = await api.get<ProductModel>(`${APIRoute.products}/${id}`);
+export const fetchProductById = async (id: string): Promise<ProductApi> => {
+  const { data } = await api.get<ProductApi>(`${APIRoute.products}/${id}`);
   return data;
 };
 
-export const fetchProductsByCategory = async (id: number, offset = 0, limit = 0): Promise<ProductModel[]> => {
-  const { data } = await api.get<ProductModel[]>(
+export const fetchProductsByCategory = async (id: number, offset = 0, limit = 0): Promise<ProductApi[]> => {
+  const { data } = await api.get<ProductApi[]>(
     `${APIRoute.categories}/${id}${APIRoute.products}?offset=${offset}&limit=${limit}`
   );
   return data;
 };
 
-export const fetchCategories = async (): Promise<TCategory[]> => {
-  const { data } = await api.get<TCategory[]>(APIRoute.categories);
+export const fetchCategories = async (): Promise<CategoryApi[]> => {
+  const { data } = await api.get<CategoryApi[]>(APIRoute.categories);
   return data;
 };
