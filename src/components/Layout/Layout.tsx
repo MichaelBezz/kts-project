ca12f0cb1df4e195730965471291d1d9a05ba19e
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-14 21:57:41 +0300

    feat: добавляет RootStore

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from 'components/Header';
+import { useQueryParamsStoreInit } from 'store/RootStore/hooks/useQueryParamsStoreInit';
 import styles from './Layout.module.scss';
 
 const Layout: React.FC = () => {
+  useQueryParamsStoreInit();
+
   return (
     <div className={styles['layout']}>
       <Header />
