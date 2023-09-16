e6a7cf3b00c2c74d3ccc8f28f7d933282338a940
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-16 15:10:43 +0300

    feat: добавляет ProductList

--- src/App/pages/ProductsPage/ProductsPage.tsx
+++ src/App/pages/ProductsPage/ProductsPage.tsx
@@ -1,36 +1,11 @@
-import { observer } from 'mobx-react-lite';
 import * as React from 'react';
-import { useSearchParams } from 'react-router-dom';
-import CardList from 'components/CardList';
-import Loader from 'components/Loader';
-import Pagination from 'components/Pagination';
 import Text from 'components/Text';
-import ProductsStore from 'store/ProductsStore';
-import { useLocalStore } from 'store/hooks/useLocalStore';
 // import Filter from './components/Filter';
+import ProductList from './components/ProductList';
 // import Search from './components/Search';
 import styles from './ProductsPage.module.scss';
 
 const ProductsPage: React.FC = () => {
-  const productsStore = useLocalStore(() => new ProductsStore());
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  React.useEffect(() => {
-    productsStore.getProductCount();
-    productsStore.getProducts();
-  }, [productsStore]);
-
-  const handelPaginationChange = React.useCallback((page: number) => {
-    searchParams.set('page', `${page}`);
-    setSearchParams(searchParams);
-
-    productsStore.setCurrentPage(page);
-  }, [searchParams, setSearchParams, productsStore]);
-
-  if (productsStore.isLoading) {
-    return (<Loader size="general" />);
-  }
-
   return(
     <div className={styles['products-page']}>
       <div className="container">
@@ -47,26 +22,10 @@ const ProductsPage: React.FC = () => {
 
         {/* <Search className={styles['products-page__search']} /> */}
         {/* <Filter className={styles['products-page__filter']} /> */}
-
-        <CardList
-          className={styles['products-page__cards']}
-          title={'Total Product'}
-          products={productsStore.products}
-          productCount={productsStore.productCount}
-          isLoading={productsStore.isLoading}
-          loaderCount={productsStore.productLimit}
-        />
-
-        <Pagination
-          className={styles['products-page__pagination']}
-          currentPage={productsStore.currentPage}
-          totalCount={productsStore.productCount ?? 0}
-          pageSize={productsStore.productLimit}
-          onPageChange={handelPaginationChange}
-        />
+        <ProductList className={styles['products-page__cards']} />
       </div>
     </div>
   );
 };
 
-export default observer(ProductsPage);
+export default ProductsPage;
