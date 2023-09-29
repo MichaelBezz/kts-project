754cc1604d1c5faec4d153ff990e068f1828d894
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2023-09-29 13:17:16 +0300

    feat: добавляет SignInForm

---
+++ src/App/pages/LoginPage/components/SingInForm/SignInForm.tsx
@@ -0,0 +1,109 @@
+import cn from 'classnames';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { observer } from 'mobx-react-lite';
+import * as React from 'react';
+import * as Yup from 'yup';
+import Text from 'components/Text';
+import Button from 'components/buttons/Button';
+import { IAuthRequest } from 'models/AuthMode';
+import { useAuthStore } from 'store/RootStore/hooks';
+import styles from './SignInForm.module.scss';
+
+export type SignInFormProps = {
+  className?: string;
+};
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string()
+    .trim()
+    .email('Enter correct email, for example: john@mail.com')
+    .required('Required'),
+  password: Yup.string()
+    .trim()
+    .min(3, 'The password must contain more than three symbols, for example: changeme')
+    .required('Required'),
+});
+
+const SignInForm: React.FC<SignInFormProps> = ({ className }) => {
+  const authStore = useAuthStore();
+
+  const handleFormSubmit = React.useCallback((value: IAuthRequest) => {
+    authStore.setAuthRequest(value);
+    authStore.login();
+    authStore.setAuthRequest({ email: '', password: '' });
+  }, [authStore]);
+
+  return (
+    <div className={cn(styles['sign-in-form'], className)}>
+      {authStore.isError && (
+        <Text className={styles['sign-in-form__message']} tag="p" view="p-18" color="error">
+          Email or password entered incorrectly
+        </Text>
+      )}
+
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={SignupSchema}
+        onSubmit={(values) => {
+          handleFormSubmit(values);
+        }}
+      >
+        {({ isValid }) => (
+          <Form className={styles['sign-in-form__form']} noValidate>
+            <div className={styles['sign-in-form__field']}>
+              <label htmlFor="si-email">
+                <Text tag="p" view="p-20">Email:</Text>
+              </label>
+
+              <Field
+                className={styles['sign-in-form__input']}
+                id="si-email"
+                type="email"
+                name="email"
+                placeholder="john@mail.com"
+              />
+
+              <ErrorMessage name="email">
+                {(msg) => (
+                  <Text tag="p" view="p-14" color="error">{msg}</Text>
+                )}
+              </ErrorMessage>
+            </div>
+
+            <div className={styles['sign-in-form__field']}>
+              <label htmlFor="si-password">
+                <Text tag="p" view="p-20">Password:</Text>
+              </label>
+
+              <Field
+                className={styles['sign-in-form__input']}
+                id="si-password"
+                type="password"
+                name="password"
+                placeholder="changeme"
+                autoComplete="off"
+              />
+
+              <ErrorMessage name="password">
+                {(msg) => (
+                    <Text tag="p" view="p-14" color="error">{msg}</Text>
+                  )}
+              </ErrorMessage>
+            </div>
+
+            <Button
+              className={styles['sign-in-form__button']}
+              buttonStyle="primary"
+              type="submit"
+              disabled={!isValid}
+            >
+              Sing in
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default observer(SignInForm);
